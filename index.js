7714e97493b390e3e568fb7c70bfe38a74d0df50
Mohankumar-M24 <1726k0146@gmail.com>
2025-06-09 22:21:27 +0530

    changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
     .then(() => console.log('MongoDB connected'))
 
