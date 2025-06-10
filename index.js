ad59ee3003e672e39c89c0328e1cd371e9920b69
Mohankumar-M24 <1726k0146@gmail.com>
2025-06-10 10:25:46 +0530

    changes

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { 
+    
+    useNewUrlParser: true, 
+    
+    useUnifiedTopology: true })
 
     .then(() => console.log('MongoDB connected'))
 
@@ -21,3 +25,5 @@ app.use('/api/recipes', recipeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running"));
+
+console.log('Connecting to:', process.env.MONGO_URI);
